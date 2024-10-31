0a83b7b3a185c31b20339a6f5e62882f3bf8f9fa
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-31 12:00:43 -0500

    Part 3 done

--- controlled-forms-in-react/src/App.jsx
+++ controlled-forms-in-react/src/App.jsx
@@ -3,15 +3,18 @@
 import './App.css'
 
 // import the useState() hook
-import { useState } from 'react';
+import { useState } from 'react'
 
 const App = () => {
   // add placeholder text for the title
   // we'll use the form to update this state by the end of this lesson
-  const [title, setTitle] = useState('The full name will appear here.');
-  // declaring state with an empty string as the initial value
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [title, setTitle] = useState('The full name will appear here.')
+
+  // Replace the firstName and lastName state variables with the following:
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+  })
 
   return (
     <>
@@ -21,31 +24,41 @@ const App = () => {
       <input
           id="firstName"
           name="firstName"
-          value={firstName}
-          onChange={handleFirstNameChange}
+          value={formData.firstName} /* Dot notation to target the "formData" object */
+          onChange={handleChange}
         />
         <label htmlFor="lastName">Last Name: </label>
         <input
           id="lastName"
           name="lastName"
-          value={lastName}
-          onChange={handleLastNameChange}
+          value={formData.lastName} /* Dot notation to target the "formData" object */
+          onChange={handleChange}
         />
     </form>
   </>
-  );
+  )
 
   /* Functions */
 
-  function handleFirstNameChange(event) {
+  /* function handleFirstNameChange(event) {
     setFirstName(event.target.value)
   }
 
   function handleLastNameChange(event) {
     setLastName(event.target.value)
+  } */
+
+  function handleChange(event) {
+    /* Replaces old object with new object */
+    /* Does so by targetting parameter inside of the above HTML objects above (Nam ) */
+    /* (Broooo wtf is this??) */
+    setFormData( 
+      {... formData, [event.target.name]: event.target.value }
+    ) 
+    
   }
 
-};
+}
 
-export default App;
+export default App
 
