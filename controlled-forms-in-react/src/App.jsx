4783c309fbe89208b5d53e8e127f1412eb6e4a67
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-10-31 11:32:30 -0500

    Part 1 done

--- controlled-forms-in-react/src/App.jsx
+++ controlled-forms-in-react/src/App.jsx
@@ -3,14 +3,28 @@ import './App.css'
 
 // src/App.jsx
 
-import './App.css'
-
 const App = () => {
+  // construct the cityInput state
+  const [cityInput, setCityInput] = useState('');
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <label htmlFor="cityInput">City: </label>
+      <input id="cityInput" 
+      name="cityInput" 
+      type="text" 
+      value={cityInput} 
+      onChange={ handleChange }/> {/* Without onChange event handler, this wont update */}
+    </>
   );
-  
+
+  /* Functions */
+
+  function handleChange(event) {
+    setCityInput(event.target.value)
+  }
 };
 
 export default App;
+
+
